cf75a915bc440d7a203ef69b09c01f62e74c634b
Denis <voland2114@gmail.com>
2020-09-07 14:05:03 +0700

    chore

--- src/app/shop/components/follow-list/follow-list.component.ts
+++ src/app/shop/components/follow-list/follow-list.component.ts
@@ -10,13 +10,14 @@ import { Product } from 'src/app/model/product.model';
 import { ProductsState } from 'src/app/store/state/products.state';
 import { FollowState } from 'src/app/store/state/follow.state';
 import { map } from 'rxjs/operators';
+import { DeleteFollow } from 'src/app/store/action/follow.action';
 
 @Component({
   selector: 'app-follow-list',
   templateUrl: './follow-list.component.html',
   styleUrls: ['./follow-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-//   encapsulation: ViewEncapsulation.None,
+  //   encapsulation: ViewEncapsulation.None,
 })
 export class FollowListComponent implements OnInit {
   @Select(ProductsState.products) products$: Observable<Product[]>;
@@ -29,5 +30,7 @@ export class FollowListComponent implements OnInit {
       map(([prod, follow]) => prod.filter((p) => follow.indexOf(p.key) > -1))
     );
   }
-  load() {}
+  deleteFollow(key) {
+    this.store.dispatch(new DeleteFollow(key));
+  }
 }
